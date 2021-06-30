bd76e90f726480fe6b78e88c04e5cc7e80b1122f
Hubert <konczito33@gmail.com>
2021-07-01 01:56:56 +0200

    Updates

--- static/js/main.e2984868.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[0],{9:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),i=n(3),r=n.n(i),s=n(0);var a=function(){return Object(s.jsx)("div",{className:"App"})},u=function(t){t&&t instanceof Function&&n.e(3).then(n.bind(null,10)).then((function(e){var n=e.getCLS,o=e.getFID,c=e.getFCP,i=e.getLCP,r=e.getTTFB;n(t),o(t),c(t),i(t),r(t)}))};r.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root")),u()}},[[9,1,2]]]);
-//# sourceMappingURL=main.e2984868.chunk.js.map
